4f988b0d17d9d24051b7d00637d025b8a71acec9
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2014-05-28 14:06:39 +0200

    Comment the CyActivator class.

--- src/main/java/org/cytoscape/heinz/internal/CyActivator.java
+++ src/main/java/org/cytoscape/heinz/internal/CyActivator.java
@@ -8,19 +8,29 @@ import org.osgi.framework.BundleContext;
 import static org.cytoscape.work.ServiceProperties.*;
 import static org.cytoscape.application.swing.ActionEnableSupport.ENABLE_FOR_NETWORK;
 
-public class CyActivator extends AbstractCyActivator {
 
+/**
+ * Registers OSGi services accessible to Cytoscape.
+ */
+public class CyActivator extends AbstractCyActivator {
+	
+	/**
+	 * Register a HeinzTaskFactory as an OSGi service for Cytoscape to find.
+	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
+		
 		HeinzTaskFactory taskFactory = new HeinzTaskFactory();
 		
 		Properties properties = new Properties();
 		properties.put(TITLE, "Run Heinz");
 		properties.put(PREFERRED_MENU, "Apps");
+		// Enable the menu button only when a network has been loaded
 		properties.put(ENABLE_FOR, ENABLE_FOR_NETWORK);
 		properties.put(TOOLTIP, "Identify functional modules based on p-values");
 		
 		registerService(context, taskFactory, NetworkTaskFactory.class, properties);
+		
 	}
 
 }
