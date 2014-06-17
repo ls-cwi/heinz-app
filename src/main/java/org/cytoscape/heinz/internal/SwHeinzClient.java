054bf7d7990a67c3b709b91ac1a217058ae4b693
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2014-06-17 17:22:52 +0200

    Implement SwHeinzClient.retrieveResults()

--- src/main/java/org/cytoscape/heinz/internal/SwHeinzClient.java
+++ src/main/java/org/cytoscape/heinz/internal/SwHeinzClient.java
@@ -2,6 +2,8 @@ package org.cytoscape.heinz.internal;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.nio.charset.Charset;
 
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyEdge;
 
 
@@ -185,7 +188,72 @@ public class SwHeinzClient extends AbstractSwClient implements HeinzClient {
 	@Override
 	public void retrieveResults(CyTable nodeTable, String resultColumnName)
 			throws IOException, IllegalArgumentException {
-		// TODO Auto-generated method stub
+		
+		// Check if the node table has (node) SUIDs as its primary key column
+		if (
+				nodeTable.getPrimaryKey().getName() != "SUID" ||
+				nodeTable.getPrimaryKey().getType() != Long.class) {
+			throw new IllegalArgumentException(
+					"Primary key of node table (" +
+					nodeTable.getPrimaryKey().getName() +
+					") is not the SUID.");
+		}
+		
+		// retrieve the output file
+		new ClientMessage(
+				ClientMessage.TYPE_GET_OUTPUT,
+				"0",
+				null).send(outputStream);
+		ServerMessage response = ServerMessage.receive(inputStream);
+		if (response.getType() != ServerMessage.TYPE_OUTPUT) {
+			throw new IOException("Invalid response from server.");
+		}
+		// decode the contents as text
+		String outputFileContents = new String(
+				response.getPayload(),
+				Charset.forName("US-ASCII"));
+		
+		// identify and if necessary create the column to store the results in
+		CyColumn resultColumn = nodeTable.getColumn(resultColumnName);
+		// if there was no column with that name yet
+		if (resultColumn == null) {
+			// create the column
+			nodeTable.createColumn(resultColumnName, Boolean.class, false);
+		// check if the (existing) column is of the right type
+		} else if (resultColumn.getType() != Boolean.class) {
+			throw new IllegalArgumentException(
+					"Selected output column is not of type Boolean.");
+		}
+		
+		// wrap the string in an object that allows reading line by line
+		BufferedReader reader = new BufferedReader(
+				new StringReader(outputFileContents));
+		String line;
+		// for each line in the file
+		while ((line = reader.readLine()) != null) {
+			// if this is a comment line
+			if (line.startsWith("#")) {
+				// skip to the next line
+				continue;
+			}
+			
+			// Split on any (sequence of) whitespace characters
+			String[] fields = line.split("\\s+");
+			if (fields.length != 2) {
+				throw new IOException(
+						"File received from server does not have two columns");
+			}
+			
+			// parse the first field (the ID) as a long integer
+			long nodeSuid = Long.parseLong(fields[0]);
+			// parse the second field as a floating-point number and test it:
+			// it’s a score if the node is in the module or NaN if not
+			boolean inModule = !Double.isNaN(Double.parseDouble(fields[1]));
+			
+			// set the result column item for this node
+			nodeTable.getRow(nodeSuid).set(resultColumnName, inModule);
+			
+		}
 		
 	}
 	
