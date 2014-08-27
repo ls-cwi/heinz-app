3dda85d84eb0d3fc478e8d45d846c27e344bfbe0
Fedde Schaeffer <fedde.schaeffer@gmail.com>
2014-08-27 17:37:07 +0200

    Make the GO enrichment task load terms’ names and namespaces into RAM
    Skipping over the dependency issues for now.

--- src/main/java/org/cytoscape/heinz/internal/GoEnrichmentTask.java
+++ src/main/java/org/cytoscape/heinz/internal/GoEnrichmentTask.java
@@ -2,11 +2,15 @@ package org.cytoscape.heinz.internal;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Map;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.cytoscape.task.AbstractTableTask;
 import org.cytoscape.work.TaskMonitor;
@@ -26,10 +30,91 @@ import org.bridgedb.IDMapperException;
 @SuppressWarnings("deprecation")
 public class GoEnrichmentTask extends AbstractTableTask {
 	
+	private enum GoNamespace {
+		BIOLOGICAL_PROCESS,
+		CELLULAR_COMPONENT,
+		MOLECULAR_FUNCTION,
+		EXTERNAL
+	}
+	
+	private static class GoTerm {
+		
+		public final GoNamespace namespace;
+		public final String name;
+		
+		public GoTerm(GoNamespace namespace, String name) {
+			this.namespace = namespace;
+			this.name = name;
+		}
+		
+	}
+	
+	private static Map<String, GoTerm> parseOboFile(InputStream oboFile)
+			throws IOException {
+		Map<String, GoTerm> goTermMap = new HashMap<String, GoTerm>();
+		// loop over the lines of the file
+		BufferedReader reader = 
+				new BufferedReader(new InputStreamReader(oboFile, "UTF-8"));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			// when the start of a term entry is encountered
+			if (line.equals("[Term]")) {
+				// empty the variables for the fields
+				String id = null;
+				String name = null;
+				GoNamespace namespace = null;
+				// loop over the lines of the entry
+				while ((line = reader.readLine()) != null) {
+					// if this line marks the end of the entry
+					if (line.equals("")) {
+						// break out of the loop
+						break;
+					}
+					// split the field into the name and the value
+					String[] field = line.split(": ", 2);
+					// check if the field is one of the required ones and handle it
+					if (field[0].equals("id")) {
+						id = field[1];
+					} else if (field[0].equals("name")) {
+						name = field[1];
+					} else if (field[0].equals("namespace")) {
+						if (field[1].equals("biological_process")) {
+							namespace = GoNamespace.BIOLOGICAL_PROCESS;
+						} else if (field[1].equals("cellular_component")) {
+							namespace = GoNamespace.CELLULAR_COMPONENT;
+						} else if (field[1].equals("molecular_function")) {
+							namespace = GoNamespace.MOLECULAR_FUNCTION;
+						} else {
+							throw new IOException(
+									"Unexpected namespace in ontology file: " + 
+									field[1]);
+						}
+					}
+				}
+				// check if all required fields have been read for the entry
+				if (id == null) {
+					throw new IOException(
+							"Encountered entry without id in ontology file");
+				} else if (name == null) {
+					throw new IOException(
+							"Encountered entry without name in ontology file");
+				} else if (namespace == null) {
+					throw new IOException(
+							"Encountered entry without namespace in ontology file");
+				}
+				// now the entry has been parsed, add it to the map
+				goTermMap.put(id, new GoTerm(namespace, name));
+			}
+		}
+		
+		return goTermMap;
+	}
+	
 	private final String bridgeDbFileName;
 	private final String idColumnName;
 	private final String idType;
 	private final String moduleColumnName;
+	private Map<String, GoTerm> goTermMap;
 	
 	/**
  	 * Initialise the task, obtaining required parameters.
@@ -64,7 +149,7 @@ public class GoEnrichmentTask extends AbstractTableTask {
     	}
     	return availableDbNames.toArray(new String[0]);
     }
-
+    
 	/**
      * Find GO terms enriched in the network and add them as group nodes.
      * 
@@ -75,11 +160,15 @@ public class GoEnrichmentTask extends AbstractTableTask {
 		
 		// Give the task a title (shown in status monitor)
 		taskMonitor.setTitle("GO Enrichment");
-				
-		taskMonitor.setStatusMessage(
-				"Loading the Bridge Derby database");
 		
-		// TODO fix the dependencies for Bridge Derby databases to make this work
+		taskMonitor.setStatusMessage(
+				"Loading the Gene Ontology");
+		
+		goTermMap = parseOboFile(getClass().getResourceAsStream("/data/go.obo"));
+		
+		taskMonitor.setStatusMessage(
+				"Loading the selected Bridge Derby database");
+		
 		if (false) {
 		// Load the driver for loading the database
     	try {
@@ -147,6 +236,9 @@ public class GoEnrichmentTask extends AbstractTableTask {
 			row.set("all terms", rowTermStrings);
 		}
 		}
+		
+		taskMonitor.setStatusMessage(
+				"Separating terms by namespace");
 		
 		// TODO create a group node for each term occurring in the module
 		
